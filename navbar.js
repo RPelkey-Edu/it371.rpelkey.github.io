41df3275f9d96eff5e191c7e62775ed1deed9373
RPelkey-Edu <rpelkey13@gmail.com>
2025-10-05 23:32:49 -0400

    update navbar javascript

--- navbar.js
+++ navbar.js
@@ -4,5 +4,13 @@ window.addEventListener("DOMContentLoaded", function () {
     .then((response) => response.text())
     .then((html) => {
       document.getElementById("navbar-container").innerHTML = html;
+      // Highlight current page link
+      var links = document.querySelectorAll("#navbar-container nav ul li a");
+      var current = window.location.pathname.split("/").pop();
+      links.forEach(function (link) {
+        if (link.getAttribute("href") === current) {
+          link.classList.add("active-nav");
+        }
+      });
     });
 });
